db87b7a095b8672c5e4d9260d8a66eb8ec6b5d05
Angga Hermawan <53201204+angga-dev@users.noreply.github.com>
2022-05-02 19:42:26 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,380 @@
+<?php 
+  $title = "Home";
+  include ("./header.php");
+?>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>No Coding No Life</h1>
+      <p>Seorang <span class="typed" data-typed-items="Programmer, Rebahaners, VVibu"></span></p>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Tentang Saya</h2>
+          <p>Tidak meragukan client meski permintaan nya aneh-aneh, itulah salah satu moto saya dalam membuat sebuah aplikasi. Permintaan sesulit dan serumit apapun akan saya coba membuatnya, saya tidak akan pernah berhenti mencoba sampai titik dimana mustahil dikerjakan.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="assets/img/angga.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>Fullstack Web &amp; Mobile Developer.</h3>
+            <p class="fst-italic">
+              Tak kenal maka tak sayang, berikut sedikit informasi terkait data diri saya agar anda bisa lebih mengenal saya.
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Kota:</strong> <span>Cimahi</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Telp:</strong> <span>0821 2702 4434</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>mxm.angga@gmail.com</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Umur:</strong> <span>21th</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Sifat:</strong> <span>Humble</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Hobi:</strong> <span>Membaca Novel</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Resume Section ======= -->
+    <section id="resume" class="resume section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Resume</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-up">
+            <h3 class="resume-title">Pendidikan</h3>
+            <div class="resume-item">
+              <h4>SD Negeri Cibeber 4</h4>
+              <h5>2007 - 2013</h5>
+            </div>
+            <div class="resume-item">
+              <h4>SMP Negeri 9 Cimahi</h4>
+              <h5>2013 - 2016</h5>
+            </div>
+            <div class="resume-item">
+              <h4>SMK Negeri 11 Bandung</h4>
+              <h5>2016 - 2019</h5>
+              <p><em>Rekayasa Perangkat Lunak</em></p>
+            </div>
+          </div>
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">Pengalaman Kerja</h3>
+            <div class="resume-item">
+              <h4>Start up technopark</h4>
+              <h5>2018 - 2019</h5>
+              <p><em>Intership SMKN 11 Bandung </em></p>
+              <ul>
+                <li>Membuat aplikasi toko online dengan framework CI 3.</li>
+                <li>Membuat aplikasi event organizer berbasis IoT. </li>
+              </ul>
+            </div>
+            <div class="resume-item">
+              <h4>Programmer</h4>
+              <h5>Juni 2019 -  Juli 2021</h5>
+              <p><em>Maxxima Innovative Engineering</em></p>
+              <ul>
+                <li>Bertanggung jawab penuh dalam 5 project individu.</li>
+                <li>Berkontribusi lebih dari 20+ project tim</li>
+                <li>Menangani berbagai macam project seperti games development, web apps & service mobile apps</li>
+                <li>Deploy aplikasi ke server produksi</li>
+              </ul>
+            </div>
+            <div class="resume-item">
+              <h4>Electronic Data Processing</h4>
+              <h5>Agustus 2021 - sekarang</h5>
+              <p><em>PT Graha Seribusatu Jaya</em></p>
+              <ul>
+                <li>Berkontribusi dalam 5 project tim</li>
+                <li>Membuat fullstack aplikasi (web, api, mobile)</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
+
+    <!-- ======= Skills Section ======= -->
+    <section id="skills" class="skills">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Kemampuan</h2>
+          <p>Non Framework & Framework</p>
+        </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-6" data-aos="fade-up">
+
+            <div class="progress">
+              <span class="skill">HTML <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">CSS <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">JavaScript / JQuery <i class="val">85%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <br><br>
+
+            <div class="progress">
+              <span class="skill">Laravel <i class="val">85%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">CodeIgniter <i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <div class="progress">
+              <span class="skill">PHP <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Dart <i class="val">70%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">SQL <i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <br><br>
+
+            <div class="progress">
+              <span class="skill">Flutter <i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+            
+            <div class="progress">
+              <span class="skill">Leaflet JS <i class="val">70%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Skills Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Portfolio</h2>
+          <p>Berikut adalah beberapa project yang pernah saya kerjakan.</p>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">All</li>
+              <li data-filter=".filter-web">Web</li>
+              <li data-filter=".filter-mobile">Mobile</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/thumb/e-central_frontend.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/e-central/e-central_frontend-1.png" data-gallery="portfolioList" class="portfolio-lightbox" title=""><i class="bx bx-image-alt"></i></a>
+                <a href="javascript:void(0);" title="Detail Project" onclick="alert('Fitur sedang dalam tahap development.')"><i class="bx bx-info-circle"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/thumb/inaexport_frontend.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/inaexport/inaexport_frontend.png" data-gallery="portfolioList" class="portfolio-lightbox" title=""><i class="bx bx-image-alt"></i></a>
+                <a href="javascript:void(0);" title="Detail Project" onclick="alert('Fitur sedang dalam tahap development.')"><i class="bx bx-info-circle"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/thumb/e-patroli_admin.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/e-patroli/e-patroli_admin-1.png" data-gallery="portfolioList" class="portfolio-lightbox" title=""><i class="bx bx-image-alt"></i></a>
+                <a href="javascript:void(0);" title="Detail Project" onclick="alert('Fitur sedang dalam tahap development.')"><i class="bx bx-info-circle"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/thumb/presencia_admin.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/presencia/presencia_admin-1.png" data-gallery="portfolioList" class="portfolio-lightbox" title=""><i class="bx bx-image-alt"></i></a>
+                <a href="javascript:void(0);" title="Detail Project" onclick="alert('Fitur sedang dalam tahap development.')"><i class="bx bx-info-circle"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/thumb/smartwatch_admin.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/smartwatch/smartwatch_admin-3.png" data-gallery="portfolioList" class="portfolio-lightbox" title=""><i class="bx bx-image-alt"></i></a>
+                <a href="javascript:void(0);" title="Detail Project" onclick="alert('Fitur sedang dalam tahap development.')"><i class="bx bx-info-circle"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/thumb/windity-fronend.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/windity/windity-fronend.png" data-gallery="portfolioList" class="portfolio-lightbox" title=""><i class="bx bx-image-alt"></i></a>
+                <a href="javascript:void(0);" title="Detail Project" onclick="alert('Fitur sedang dalam tahap development.')"><i class="bx bx-info-circle"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/thumb/wms-login.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/wms/wms-login-1.png" data-gallery="portfolioList" class="portfolio-lightbox" title=""><i class="bx bx-image-alt"></i></a>
+                <a href="javascript:void(0);" title="Detail Project" onclick="alert('Fitur sedang dalam tahap development.')"><i class="bx bx-info-circle"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-mobile">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/thumb/e-central_mobile.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/e-central/e-central_mobile-2.png" data-gallery="portfolioList" class="portfolio-lightbox" title=""><i class="bx bx-image-alt"></i></a>
+                <a href="javascript:void(0);" title="Detail Project" onclick="alert('Fitur sedang dalam tahap development.')"><i class="bx bx-info-circle"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-mobile">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/thumb/presencia_mobile.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/img/portfolio/presencia/presencia_mobile-1.png" data-gallery="portfolioList" class="portfolio-lightbox" title=""><i class="bx bx-image-alt"></i></a>
+                <a href="javascript:void(0);" title="Detail Project" onclick="alert('Fitur sedang dalam tahap development.')"><i class="bx bx-info-circle"></i></a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Hubungi Saya</h2>
+        </div>
+
+        <div class="row" data-aos="fade-in">
+
+          <div class="col-lg-12 mt-5 mt-lg-0 d-flex align-items-stretch">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Nama</label>
+                  <input type="text" name="name" class="form-control" id="name" required>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Email</label>
+                  <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="name">Subjek</label>
+                <input type="text" class="form-control" name="subject" id="subject" required>
+              </div>
+              <div class="form-group">
+                <label for="name">Pesan</label>
+                <textarea class="form-control" name="message" rows="10" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Pesan anda berhasil dikirim.</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+<?php 
+  include ("./footer.php");
+?>
